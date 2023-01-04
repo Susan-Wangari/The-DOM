45cd26485d7ec04d090e072e070da89c8cf1d185
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2023-01-04 13:35:27 +0300

    learning the DOM and manipulation

--- createElement.js
+++ createElement.js
@@ -3,3 +3,15 @@
 const para = document.createElement('p');
 //add content
 para.textContent ='I love my life';
+
+//add a list of languages to the document body
+let languages = document.createElement('ol');
+document.body.appendChild(languages);
+
+let lang1 = document.createElement('li');
+lang1.textContent = "JavaScript";
+languages.appendChild(lang1);
+
+let lang2 = document.createElement('li');
+lang2.textContent="Python";
+languages.appendChild(lang2);
